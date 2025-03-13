2327b9f8723d5d1765da6ab003c0176e978106fd
OneDevShahan <onedevshahan@gmail.com>
2025-03-13 12:45:35 +0530

    Updates

--- assets/Page11-D7Nkc30v.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Cj6HsWhe.js";function r(){return e.jsx("div",{children:e.jsx("h1",{className:"dark:text-white",children:"Page 11"})})}export{r as default};
---
+++ assets/ScrollToTop-BDjr9Hzo.js
@@ -0,0 +1,1 @@
+import{u as t,r}from"./index-CfOWARvi.js";const a=()=>{const{pathname:o}=t();return r.useEffect(()=>{window.scrollTo({top:0,behavior:"smooth"})},[o]),null};export{a as default};
